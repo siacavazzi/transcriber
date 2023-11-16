cf3de2da6fa715e740f85ee1d1712016929b08d6
PaulH123 <hebertp@lafayette.edu>
2023-11-16 14:00:31 -0500

    added more objects to the home screen

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,16 +3,25 @@ import Button from '../components/Button';
 
 const Home: React.FC = () => {
   const handleClick = () => {
-    alert('Button Clicked!');
+    alert('Button Clicked!'); // this is a place holder, when the button is clicked it should begin to record
   };
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-6">Welcome to Tailwind CSS with TypeScript</h1>
+        <h1 className="text-4xl font-bold mb-6">Welcome to Transcriber Beta! press the button to begin recording your meeting </h1>
         <Button 
-          onClick={handleClick}>Click me!
+          onClick={()=>handleClick()}>Record!
         </Button>
+      </div>
+
+      {/*prompt and recording box*/}
+      <div className="container mx-auto text-center">
+        <input
+          type="text"
+          className="border p-2 mt-4"
+          placeholder="Type something..."
+        />
       </div>
     </div>
   );
