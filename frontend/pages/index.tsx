18e7d8cf3827807dec5d06e9525ebbbf486a2ddb
PaulH123 <hebertp@lafayette.edu>
2023-11-16 15:55:56 -0500

    playing with layout

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,25 +3,36 @@ import Button from '../components/Button';
 
 const Home: React.FC = () => {
   const handleClick = () => {
-    alert('Button Clicked!'); // this is a place holder, when the button is clicked it should begin to record
+    alert('Button Clicked!');                                   // this is a place holder, when the button is clicked it should begin to record
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
+                                                    
+    <div className="flex items-center justify-center h-screen"> {/*header*/}
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-6">Welcome to Transcriber Beta! press the button to begin recording your meeting </h1>
-        <Button 
-          onClick={()=>handleClick()}>Record!
+      </div>
+
+      <div className="container mx-auto text-center">           {/*prompt and recording box*/}
+
+        <h3
+          className="border p-2 mt-4">
+          Enter Prompt Here to optimize recording: 
+          <input 
+                                                           
+          type="text"
+          className="border p-2"
+          placeholder="Type something..."
+          />
+        </h3>
+
+        <Button                                                 // call the back end funtion to start recording 
+          onClick={()=>handleClick()}>Record! 
         </Button>
       </div>
 
-      {/*prompt and recording box*/}
-      <div className="container mx-auto text-center">
-        <input
-          type="text"
-          className="border p-2 mt-4"
-          placeholder="Type something..."
-        />
+      <div className="fixed bottom-0 left-0 right-0 bg-gray-100 p-4 min-h-screen"> 
+        This is a gray box at the bottom of the screen.
       </div>
     </div>
   );
